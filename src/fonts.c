6525f14a19d01cc515685ed0262a6520a880d9a9
Nick Young <nickyoungweb@gmail.com>
2026-09-11 14:10:43 -0600

    Start setting up files and functions for fonts

---
+++ src/fonts.c
@@ -0,0 +1,11 @@
+#include <ssd1306/fonts.h>
+
+const ssd1306_font_config_t SSD1306_FONT_DEFAULT_CONFIG = {
+    .letter_space = 1,
+    .word_space   = 4,
+    .line_height  = 5
+};
+
+void ssd1306_font_init(ssd1306_font_config_t *config) {
+    // TODO
+}
